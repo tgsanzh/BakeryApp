f7a18fa85df7c9f589e42bc5c6ac9252aa49f2ee
Sanzhar Tursynbay <tgsanzh@gmail.com>
2025-06-17 08:46:14 +0500

    req

--- Backend/backend/db.py
+++ Backend/backend/db.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine("mysql+mysqlconnector://root:iYNDMISGpOlvFlewQEhKAVrKjaTIpBNR@shuttle.proxy.rlwy.net:52803/railway")
+# mysql+mysqlconnector://root:iYNDMISGpOlvFlewQEhKAVrKjaTIpBNR@shuttle.proxy.rlwy.net:52803/railway
+# mysql+pymysql://root:qaws123!@localhost:3306/bakeryapp
 session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
